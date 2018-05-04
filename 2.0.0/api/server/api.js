057b37f6e850c3cfc1729317f4c4317350d5d264
Maxime Béguin <maxime.beguin@veo-labs.com>
2018-05-04 15:03:43 +0200

    Auto-generated documentation for version 2.0.0

---
+++ 2.0.0/api/server/api.js
@@ -0,0 +1,50 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "BROWSERS_MESSAGES",
+        "BrowserPilot",
+        "BrowserSocketController",
+        "Cache",
+        "Device",
+        "DeviceProvider",
+        "ERRORS",
+        "Group",
+        "GroupProvider",
+        "ManagePlugin",
+        "Manageable",
+        "ManageableProvider",
+        "Manager",
+        "ScheduleManager",
+        "browsersPilotManager"
+    ],
+    "modules": [
+        "controllers",
+        "manage",
+        "manageables",
+        "providers"
+    ],
+    "allModules": [
+        {
+            "displayName": "controllers",
+            "name": "controllers",
+            "description": "Include all plugin's controllers to handle HTTP or socket messages."
+        },
+        {
+            "displayName": "manage",
+            "name": "manage",
+            "description": "OpenVeo Manage Plugin."
+        },
+        {
+            "displayName": "manageables",
+            "name": "manageables",
+            "description": "Manageables are elements with an history and a planning."
+        },
+        {
+            "displayName": "providers",
+            "name": "providers",
+            "description": "Includes all Manage's providers."
+        }
+    ],
+    "elements": []
+} };
+});
