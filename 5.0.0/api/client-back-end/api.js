02bd0ada9e0260000090a6623c71968021d4bb5a
Maxime Béguin <maxime.beguin@veo-labs.com>
2020-11-19 10:34:02 +0100

    Auto-generated documentation for version 5.0.0

---
+++ 5.0.0/api/client-back-end/api.js
@@ -0,0 +1,25 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ManageDeviceFactory",
+        "ManageFactory",
+        "ManageGroupFactory",
+        "ManageMainController",
+        "ManageManageableController",
+        "ManageManageableDetailController",
+        "ManageManageableFactory",
+        "ovManageSortTable"
+    ],
+    "modules": [
+        "ov.manage"
+    ],
+    "allModules": [
+        {
+            "displayName": "ov.manage",
+            "name": "ov.manage",
+            "description": "Defines components responsible of the manage plugin back end pages.\n\nIt creates two back end pages. One to display the list of devices and groups, the other\none to display the list of devices inside a group."
+        }
+    ],
+    "elements": []
+} };
+});
