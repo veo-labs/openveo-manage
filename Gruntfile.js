f5946ae38dd34828a49d90169486ede0e57b931a
Maxime Béguin <maxime.beguin@veo-labs.com>
2021-09-30 08:22:32 +0200

    dependencies: remove grunt-contrib-watch
    Watching for changes in client back office files
    to automatically rebuild was performed by
    dependency grunt-contrib-watch.
    As @openveo/api now has its own watcher ovWatch,
    it is used to watch for changes instead of
    grunt-contrib-watch.
    Watch command has not changed and still is
    `npm run watch`.
    Note that conf.js file is not longer watched.

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,6 @@ module.exports = function(grunt) {
   grunt.initConfig(config);
   grunt.config.merge(loadConfig('./tasks'));
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-yuidoc');
